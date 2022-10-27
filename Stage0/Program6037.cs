dc442a33700e6744a6596553b7a14c38a9570c9e
SivanLevi100 <sivanlevi94@gmail.com>
2022-10-27 18:22:50 +0300

    Initial code+ welcome functions

---
+++ Stage0/Program6037.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//using System;
+
+namespace Stage0
+{
+    partial class program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6037();
+            Welcome3889();
+            Console.ReadKey();
+        }
+        static partial void Welcome3889();
+        private static void Welcome6037()
+        {
+            string sen2;
+            Console.Write("Enter your name: ");
+            sen2 = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", sen2);
+        }
+    }
+}
+
+
