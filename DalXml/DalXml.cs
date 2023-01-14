d495c707df8fba97df5027d0cf037e66265c0ee1
SivanLevi100 <sivanlevi94@gmail.com>
2023-01-14 21:56:02 +0200

    14.1 - stage6 XMl

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,22 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+sealed internal class DalXml : IDal
+{
+    private static IDal instance = new DalXml();
+    public static IDal Instance { get { return instance; } }
+    private DalXml() { }
+
+    public IProduct Product => new Dal.Product();
+    public IOrder Order => new Dal.Order();
+    public IOrderItem OrderItem => new Dal.OrderItem();
+
+}
